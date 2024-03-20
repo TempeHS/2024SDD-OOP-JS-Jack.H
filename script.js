99fd69a57714f518998c1ad28424cbb39badcdd0
Jack-Hunter14 <136764964+Jack-Hunter14@users.noreply.github.com>
2024-03-20 12:02:48 +0000

    Finished oop vid

--- script.js
+++ script.js
@@ -1,4 +1,107 @@
-alert("Hello, world"); //Output to windows alert box
-document.getElementById("ScriptThisDIV").innerHTML = "Hello, world"; //Output to the DOM (Document Object Module) with the id="ScriptThisDIV"
-console.log("Hello, world"); //Output to the console (Open dev tools and look at the console)
-// test
+/* const player = {
+    name: "?",
+    hp: 100, 
+    mp: 0,
+    items: [],
+};
+
+const hanSolo = player;
+hanSolo.name = "Han Solo";
+hanSolo.hp = 80;
+hanSolo.mp = 20;
+hanSolo.items = ["Blaster"];
+
+const darthVader = player; 
+
+console.log(hanSolo);
+console.log(darthVader);*/
+
+/* const createPlayer = (name, hp, mp, items) => {
+    return {
+        name: name, 
+        hp: hp,
+        mp: mp,
+        items: items,
+    };
+};
+
+const hanSolo = createPlayer("Han Solo", 100, 10, ["Blaster"]);
+hanSolo.hp = 150;
+
+const darthVader = createPlayer("Darth Vader", 90, 20, ["Saber"]); 
+
+console.log(hanSolo);
+console.log(darthVader);*/
+
+/*class Player {
+    constructor(name, hp, mp, items) {
+        this.name = name;
+        this.hp = hp;
+        this.mp = mp;
+        this.items = items;
+    }
+}
+
+const hanSolo = new Player("Han Solo", 100, 10, ["Blaster"]);
+
+console.log(hanSolo);
+console.log(Object.getOwnPropertyNames(hanSolo));
+console.log(Object.getOwnPropertyNames(hanSolo.__proto__));
+const nader = new Player("Nader", 10, 5, [ "Computer" ]);
+console.log(nader);
+console.log(Object.getOwnPropertyNames(nader));
+console.log(Object.getOwnPropertyNames(nader.__proto__));
+
+function AnotherPlayer(name, hp, mp, items) {
+    this.name = name;
+    this.hp = hp;
+    this.mp = mp;
+    this.items = items;
+}
+
+const darthVader = new AnotherPlayer("Darth Vader", 200, 50, ["Saber"]);
+
+console.log(darthVader);
+console.log(Object.getOwnPropertyNames(darthVader));
+console.log(Object.getOwnPropertyNames(darthVader.__proto__));*/
+
+class Player {
+    age = 20;
+    constructor(name, hp, mp, items) {
+        this.name = name;
+        this.hp = hp;
+        this.mp = mp;
+        this.items = items;
+    }
+
+    speak(phrase) {
+        console.log(`${this.name} says: ${phrase}`);
+    }
+}
+
+const hanSolo = new Player("Han Solo", 100, 10, ["Blaster"]);
+hanSolo.speak("Never tell me the odds!");
+hanSolo.age = 50;
+console.log(hanSolo);
+console.log(Object.getOwnPropertyNames(hanSolo));
+console.log(Object.getOwnPropertyNames(hanSolo.__proto__));
+
+function AnotherPlayer(name, hp, mp, items) {
+    this.name = name;
+    this.hp = hp;
+    this.mp = mp;
+    this.items = items;
+    /*this.speak = function (phrase) {
+        console.log(`${this.name} says: ${phrase}`);  
+    };*/
+}
+
+AnotherPlayer.prototype.speak = function (phrase) {
+    console.log(`${this.name} says: ${phrase}`);  
+};
+
+const darthVader = new AnotherPlayer("Darth Vader", 200, 50, ["Saber"]);
+darthVader.speak("hhhhhfshhhh");
+console.log(darthVader);
+console.log(Object.getOwnPropertyNames(darthVader));
+console.log(Object.getOwnPropertyNames(darthVader.__proto__));
