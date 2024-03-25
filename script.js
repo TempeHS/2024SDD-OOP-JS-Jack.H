30e052289c32d5b5d416e0a07b4e261c71954693
Jack-Hunter14 <136764964+Jack-Hunter14@users.noreply.github.com>
2024-03-25 06:21:35 +0000

    Added space invadors and factory

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ console.log(darthVader);
 console.log(Object.getOwnPropertyNames(darthVader));
 console.log(Object.getOwnPropertyNames(darthVader.__proto__));*/
 
-class Player {
+/*class Player {
     age = 20;
     constructor(name, hp, mp, items) {
         this.name = name;
@@ -91,9 +91,9 @@ function AnotherPlayer(name, hp, mp, items) {
     this.hp = hp;
     this.mp = mp;
     this.items = items;
-    /*this.speak = function (phrase) {
+    this.speak = function (phrase) {
         console.log(`${this.name} says: ${phrase}`);  
-    };*/
+    };
 }
 
 AnotherPlayer.prototype.speak = function (phrase) {
@@ -104,4 +104,143 @@ const darthVader = new AnotherPlayer("Darth Vader", 200, 50, ["Saber"]);
 darthVader.speak("hhhhhfshhhh");
 console.log(darthVader);
 console.log(Object.getOwnPropertyNames(darthVader));
-console.log(Object.getOwnPropertyNames(darthVader.__proto__));
+console.log(Object.getOwnPropertyNames(darthVader.__proto__));*/
+
+/*var bullets = [];
+var enemies = [];
+var player;
+var screenX = screen.width;
+var screenY = screen.height;
+var playerY = screen.height - 5;
+var highScore = 0;
+
+class Player {
+    constructor(lives, position) {
+        this.lives = lives;
+        this.position = position;
+        this.score = 0;
+    }
+    move(input) {
+        this.position += input;
+    }
+    shoot() {
+        bullets[bullets.length] = new Bullet(-1, position, playerY);
+    }
+    incScore() {
+        this.score++;
+    }
+    lose() {
+        this.lives--;
+        if (this.lives == 0) {
+            gameOver();
+        }
+    }
+}
+
+class Enemy {
+    constructor(positionX, positionY, direction, speed, fireRate) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.direction = direction;
+        this.speed = speed;
+        this.fireRate = fireRate;
+        this.fireCount = fireRate;
+        this.id = enemies.length - 1;
+    }
+    update() {
+        this.positionX += this.speed * this.direction;
+        if ((this.positionX == screenX && this.direction == 1) || (this.positionX == 0 && this.direction == -1)) {
+            this.direction *= -1;
+            this.positionY += 2;
+        }
+        this.fireCount--;
+        if (this.fireCount == 0) {
+            this.fireCount = this.fireRate;
+            bullets[bullets.length] = new Bullet(1, positionX, positionY);
+        }
+    }
+}
+
+class Bullet {
+    constructor(direction, positionX, positionY) {
+        this.direction = direction;
+        this.positionY = positionY + this.direction * 4;
+        this.positionX = positionX;
+        this.id = bullets.length - 1;
+    }
+    update() {
+        this.positionY += this.direction;
+        if (this.positionY > screenY || this.positionY < 0) {
+            destroyBullet(this.id);
+        }
+        if (direction == 1 && this.positionY == playerY && player.position == this.positionX) {
+            player.lose();
+        } else if (direction == -1) {
+            for (let i=0; i<enemies.length; i++) {
+                if (this.positionY == enemies[i].positionY && this.positionX == enemies[i].positionX) {
+                    destroy(enemies[i]);
+                    player.incScore();
+                }
+            }
+        }
+    }
+}
+
+function gameOver() {
+    enemies = [];
+    bullets = [];
+    if (player.score > highScore) {
+        highScore = player.score;
+    }
+    player = "";
+}
+
+function destroy(id) {
+    enemies.slice(id);
+}
+
+function destroyBullet(id) {
+    bullets.slice(id);
+}
+
+function newGame() {
+    player = new Player(3, (screenX / 2));
+    enemies = [new Enemy(0, 5, 1, 5, 6), new Enemy(0, 8, 1, 5, 9)];
+}
+newGame();*/
+
+class EnemyFactory {
+    constructor() {
+        this.Flying = class {
+            constructor(name) {
+                this.name = name;
+            }
+            fly() {
+                console.log(this.name + " is flying");
+            }
+        }
+        this.Swimming = class {
+            constructor(name) {
+                this.name = name;
+            }
+            swim() {
+                console.log(this.name + " is swimming");
+            }
+        }
+    }
+    generateFlyingEnemy(naming) {
+        return new this.Flying(naming);
+    }
+    generateSwimmingEnemy(naming) {
+        return new this.Swimming(naming)
+    }
+}
+
+const factory = new EnemyFactory();
+const flying = factory.generateFlyingEnemy("Batman");
+console.log(flying);
+flying.fly();
+
+const swimming = factory.generateSwimmingEnemy("Aquaman");
+console.log(swimming);
+swimming.swim();
